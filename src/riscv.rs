b0a7fc9ebeae93575280fb29bfb624897d1c6987
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2022-08-16 08:27:56 +0300

    wip: start fleshing out decode trait

--- src/riscv.rs
+++ src/riscv.rs
@@ -1,77 +1,109 @@
 use bitvec::prelude::*;
 use crate::binary_value::BinaryValue;
 
-pub enum Opcode {
-    LOAD,
-    LOAD_FP,
-    custom_0,
-    MISC_MEM,
-    OP_IMM,
-    AUIPC,
-    OP_IMM_32,
-    STORE,
-    STORE_FP,
-    custom_1,
-    AMO,
-    OP,
-    LUI,
-    OP_32,
-    MADD,
-    MSUB,
-    NMSUB,
-    NMADD,
-    OP_FP,
-    custom_2__rv128,
-    BRANCH,
-    JALR,
-    JAL,
-    SYSTEM,
-    custom_3__rv128,
-    reserved,
+/// Meaning associated with a bitfield
+struct Field<'a> {
+    source: &'a BitSlice<u8>,
+    name: String,
+    description: String,
 }
 
-pub enum Instruction {
-    BEQ,
-    BNE,
-    BLT,
-    BGE,
-    BLTU,
-    BGEU,
-    JALR,
-    JAL,
-    LUI,
-    AUIPC,
-    ADDI,
-    SLLI,
-    SRLI,
-    SRAI,
-    SLTI,
-    SLTIU,
-    XORI,
-    ORI,
-    ANDI,
-    ADD,
-    SUB,
-    SLL,
-    SLT,
-    SLTU,
-    XOR,
-    SRL,
-    SRA,
-    OR,
-    AND,
-    LB,
-    LH,
-    LW,
-    LBU,
-    LHU,
-    SB,
-    SH,
-    SW,
+// TODO: use interpretation as result/option-like container for fields
+enum Interpretation<'a> {
+    Decoded(Field<'a>),
+    Defer(&'a BitSlice<u8>), // defer interpretation to the next decode pass
 }
 
-/// Struct containing all information required to display the associated
-/// RISC-V instruction in a human readable format.
+trait Decode<'a> {
+    fn decode(bs: &BitSlice) -> Vec<Interpretation<'a>>;
+}
+
+/// decode BinaryValue as itself
+impl Decode for BinaryValue {
+
+}
+
+trait RVInstructionOverlay: Decode {
+    padding: Defer,
+    opcode: OpcodeDecoder,
+}
+
+// #[repr(u8)]
+// pub enum Opcode {
+//     LOAD = 0b00000_11,
+//     LOAD_FP = 0b00001_11,
+//     custom_0 = 0b00010_11,
+//     MISC_MEM = 0b00011_11,
+//     OP_IMM = 0b00100_11,
+//     AUIPC = 0b00101_11,
+//     OP_IMM_32 = 0b00110_11,
+//     STORE = 0b01000_11,
+//     STORE_FP = 0b01001_11,
+//     custom_1 = 0b01010_11,
+//     AMO = 0b01011_11,
+//     OP = 0b01100_11,
+//     LUI = 0b01101_11,
+//     OP_32 = 0b01110_11,
+//     MADD = 0b10000_11,
+//     MSUB = 0b10001_11,
+//     NMSUB = 0b10010_11,
+//     NMADD = 0b10011_11,
+//     OP_FP = 0b10100_11,
+//     reserved = 0b10101_11,
+//     custom_2__rv128 = 0b10110_11,
+//     BRANCH = 0b11000_11,
+//     JALR = 0b11001_11,
+//     reserved = 0b11010_11,
+//     JAL = 0b11011_11,
+//     SYSTEM = 0b11100_11,
+//     reserved = 0b11101_11,
+//     custom_3__rv128 = 0b11110_11,
+// }
+// pub enum Instruction {
+//     BEQ,
+//     BNE,
+//     BLT,
+//     BGE,
+//     BLTU,
+//     BGEU,
+//     JALR,
+//     JAL,
+//     LUI,
+//     AUIPC,
+//     ADDI,
+//     SLLI,
+//     SRLI,
+//     SRAI,
+//     SLTI,
+//     SLTIU,
+//     XORI,
+//     ORI,
+//     ANDI,
+//     ADD,
+//     SUB,
+//     SLL,
+//     SLT,
+//     SLTU,
+//     XOR,
+//     SRL,
+//     SRA,
+//     OR,
+//     AND,
+//     LB,
+//     LH,
+//     LW,
+//     LBU,
+//     LHU,
+//     SB,
+//     SH,
+//     SW,
+// }
+
+
+struct Opcode {
+
+}
+
 pub struct RISCVInstruction {
 
 }
@@ -84,11 +116,22 @@ impl RISCVInstruction {
             }
             32 => {
                 // RV32I, RV64I
-                let opcode = &binval.bits[0..7];
+                let opcode = &binval.bits[0..7].load::<u8>();
+                let funct3 = &binval.bits[12..15].load::<u8>();
+                let funct7 = &binval.bits[25..32].load::<u8>();
                 println!("opcode {:#?}", opcode);
                 
                 match opcode {
                     // TODO: how to match BitSlice values?
+                    0b00000_11 => { // LOAD
+                        todo!()
+                    },
+                    0b00001_11 => { // LOAD-FP
+                        todo!()
+                    },
+                    0b01101_11 => panic!("LUI"),
+                    0b00101_11 => panic!("AUIPC"),
+                    128.. => unreachable!(),
                     _ => todo!(),
                 }
             },
@@ -96,5 +139,24 @@ impl RISCVInstruction {
                 Err(String::from(format!("Invalid number of bits for a RISC-V instruction: {}", binval.len())))
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::binary_value::*;
+
+    // TODO: create assert macro for BinaryValue types
+    #[test]
+    fn binary_decode_works() {
+        let binval = BinaryValue::from("0x1234").unwrap();
+
+    }
+
+    #[test]
+    fn auipc_decode_works() {
+        let result = BinaryValue::from("0x20000197").unwrap();
+
     }
 }
